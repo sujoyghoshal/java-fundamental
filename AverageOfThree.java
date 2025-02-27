1165c19655abbddb2a1134bbf38ac5d96acd60db
Sujoy Ghoshal <92462200+sujoyghoshal@users.noreply.github.com>
2025-02-27 14:01:06 +0530

    Update AverageOfThree.java

--- AverageOfThree.java
+++ AverageOfThree.java
@@ -7,6 +7,5 @@ public class AverageOfThree {
         double num2 = scanner.nextDouble();
         double num3 = scanner.nextDouble();
         System.out.println((num1 + num2 + num3) / 3);
-        scanner.close();
     }
 }
