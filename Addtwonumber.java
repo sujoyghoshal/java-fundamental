fa4b1ba2ff94fdb39e96f6b138f717d274e995d1
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-02-27 11:50:10 +0530

    AddingTwo number code

---
+++ Addtwonumber.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Addtwonumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int ans=x+y;
+	    System.out.print(ans);
+    }
+}
