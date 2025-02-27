82f172fc7e5616a2d58f14c9b03f48f20f0b9a29
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-02-27 11:27:53 +0530

    adding CelsiusToFahrenheit code

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double celsius = scanner.nextDouble();
+        System.out.println((celsius * 9/5) + 32);
+        scanner.close();
+    }
+}
